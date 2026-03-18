b269f9f6de595915e2c3411f4d7ebee51b4342f1
mariainesdff <mariaines.dff@gmail.com>
2026-03-18 15:09:10 +0100

    WIP blueprint

--- HassePrinciple.lean
+++ HassePrinciple.lean
@@ -1,2 +1,8 @@
-import HassePrinciple.QuadraticForm.Basic
-import HassePrinciple.QuadraticForm.HasseMinkowski
+module
+
+public import HassePrinciple.Padics.Squares
+public import HassePrinciple.QuadraticForm.Basic
+public import HassePrinciple.QuadraticForm.HasseMinkowski
+public import HassePrinciple.QuadraticForm.HighRank
+public import HassePrinciple.QuadraticForm.RankFour
+public import HassePrinciple.QuadraticForm.RankThree
