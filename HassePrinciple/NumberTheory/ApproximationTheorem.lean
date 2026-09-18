0e4ea5db0df15d9f850b897772bf12598a384b90
NirvanaC93 <58878663+NirvanaC93@users.noreply.github.com>
2026-09-18 13:09:29 +0200

    wip

--- HassePrinciple/NumberTheory/ApproximationTheorem.lean
+++ HassePrinciple/NumberTheory/ApproximationTheorem.lean
@@ -6,17 +6,15 @@ Authors: Nirvana Coppola, María Inés de Frutos-Fernández
 module
 
 public import Mathlib.NumberTheory.Padics.PadicNumbers
---public import Mathlib.NumberTheory.PrimeCounting
+public import Mathlib.Topology.Algebra.IsOpenUnits
 
 /-! # Approximation theorem. -/
 
 @[expose] public section
 
-
 noncomputable section
 
 namespace Rat
-
 
 local instance (p : Nat.Primes) : Fact (Nat.Prime p) :=
   fact_iff.mpr p.2
@@ -29,17 +27,6 @@ theorem approximation' {S : Finset Nat.Primes} {ε : ℝ} (hε : ε > 0)
     (y : ℝ × (Π p : S, ℚ_[p])) :
     ∃ x : ℚ, ‖y.1 - x‖ + Finset.sum (Finset.attach S) (fun n ↦ ‖y.2 n - x‖) < ε := by
   sorry
-
-/-- The finite embedding of ℚ into the product of the completions of ℚ at a finite set of places
-(which includes ℝ). -/
-abbrev finiteEmbedding (S : Finset Nat.Primes) (x : ℚ) : ℝ × (Π p : S, ℚ_[p]) :=
-  ⟨algebraMap ℚ ℝ x, fun p ↦ (algebraMap ℚ ℚ_[p]) x⟩
-
-/-- The approximation theorem can be restated as saying that the finite embedding is dense. -/
-theorem approximation (S : Finset Nat.Primes) :
-    Dense (Set.range (finiteEmbedding S)) := by
-  sorry
-
 
 /-- The finite embedding of ℚ into the product of the completions of ℚ at a finite set of places
 (which includes ℝ). -/
@@ -63,9 +50,9 @@ def ContinuousMulEquiv.prodUnits (M N : Type*) [Monoid M] [TopologicalSpace M]
   __ := MulEquiv.prodUnits
   continuous_toFun := Continuous.prodMk (Units.continuous_map continuous_fst)
     (Units.continuous_map continuous_snd)
-  continuous_invFun := Units.continuous_iff.mpr 
-    ⟨continuous_prodMk.mpr ⟨Units.continuous_val.comp continuous_fst, 
-        Units.continuous_val.comp continuous_snd⟩, 
+  continuous_invFun := Units.continuous_iff.mpr
+    ⟨continuous_prodMk.mpr ⟨Units.continuous_val.comp continuous_fst,
+        Units.continuous_val.comp continuous_snd⟩,
       continuous_prodMk.mpr ⟨Units.continuous_coe_inv.comp continuous_fst,
         Units.continuous_coe_inv.comp continuous_snd⟩⟩
 
@@ -107,48 +94,10 @@ theorem approximation_units (S : Finset Nat.Primes) :
     exact Units.ne_zero u hur
   use Units.mk0 r hr0
   simp [← Units.val_inj, hur]
-  have := approximation S
-  simp only [Dense, closure, Set.range, finiteEmbedding, eq_ratCast, Set.mem_sInter,
-    Set.mem_ofPred_eq, and_imp, Prod.forall, finiteEmbedding''] at this ⊢
-  intro a b T hclosed
-  specialize this a.val (fun p ↦ b p)
-
-
-
-
-
-
-
-
-
-  -- set f := fun x : T ↦ ((x.val.1.val, fun p ↦ (x.val.2 p).val) : ℝ × (Π p : S, ℚ_[p]))
-  -- set T' : Set (ℝ × ((p : S) → ℚ_[p])) :=
-  --   closure (Set.range f)
-  -- specialize this T'
-  -- have later : T' ∈ {t | IsClosed t ∧ Set.range (finiteEmbedding S) ⊆ t} := sorry
-  -- specialize this later
-
-
-
-
-
-  sorry
-  --
-  -- have hclosed' : IsClosed T' := by simp [T']
-  -- specialize this hclosed'
-  -- have hsset' : Set.range (finiteEmbedding S) ⊆ T' := sorry
-  -- specialize this hsset'
-  -- simp only [closure_eq_self_union_frontier, Set.range, Subtype.exists, exists_prop, Prod.exists,
-  --   Set.mem_union, Set.mem_ofPred_eq, Prod.mk.injEq, T', f] at this
-  -- rcases this with self | front
-  -- · obtain ⟨a',b',h,h',h''⟩ := self
-  --   rw_mod_cast [← h']
-  --   have : b = fun p ↦ b p := by simp
-  --   rw_mod_cast [this]
-
-  --   sorry
-  -- · sorry
-
-
 
 end Rat
+
+
+
+
+/-The code needs to be cleaned up (golf, organize namespaces, move general lemmas to right file, etc), but I will leave this to you. To fill in the sorry in approximation_units, you will also need a general product version of the IsOpenUnits instance. I think this should follow similarly to the prod case (and note that Mathlib already contains ContinuousMulEquiv.piUnits ).-/
