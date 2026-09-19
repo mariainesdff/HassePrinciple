9f88787a6954236f10e4ed4fa7b3975d95699a96
NirvanaC93 <nirvanac93@gmail.com>
2026-09-19 02:35:23 +0200

    added HassePrinciple.ForMathlib.Topology.Algebra.Group.Units

--- HassePrinciple/NumberTheory/ApproximationTheorem.lean
+++ HassePrinciple/NumberTheory/ApproximationTheorem.lean
@@ -6,7 +6,7 @@ Authors: Nirvana Coppola, María Inés de Frutos-Fernández
 module
 
 public import Mathlib.NumberTheory.Padics.PadicNumbers
-public import Mathlib.Topology.Algebra.IsOpenUnits
+public import HassePrinciple.ForMathlib.Topology.Algebra.Group.Units
 
 /-! # Approximation theorem. -/
 
@@ -42,35 +42,6 @@ theorem approximation (S : Finset Nat.Primes) :
     Dense (Set.range (finiteEmbedding S)) := by
   sorry
 
-open Topology
-
-def ContinuousMulEquiv.prodUnits (M N : Type*) [Monoid M] [TopologicalSpace M]
-    [Monoid N] [TopologicalSpace N] :
-    (M × N)ˣ ≃ₜ* Mˣ × Nˣ where
-  __ := MulEquiv.prodUnits
-  continuous_toFun := Continuous.prodMk (Units.continuous_map continuous_fst)
-    (Units.continuous_map continuous_snd)
-  continuous_invFun := Units.continuous_iff.mpr
-    ⟨continuous_prodMk.mpr ⟨Units.continuous_val.comp continuous_fst,
-        Units.continuous_val.comp continuous_snd⟩,
-      continuous_prodMk.mpr ⟨Units.continuous_coe_inv.comp continuous_fst,
-        Units.continuous_coe_inv.comp continuous_snd⟩⟩
-
-theorem ContinuousMulEquiv.prodUnits_isOpenEmbedding {M N : Type*} [Monoid M] [TopologicalSpace M]
-    [Monoid N] [TopologicalSpace N] :
-    IsOpenEmbedding (ContinuousMulEquiv.prodUnits M N) := by
-  exact IsOpenEmbedding.of_continuous_injective_isOpenMap
-    (map_continuous (ContinuousMulEquiv.prodUnits M N))
-    (ContinuousMulEquiv.prodUnits M N).injective
-    (ContinuousMulEquiv.prodUnits M N).toHomeomorph.isOpenMap
-
-instance (M N : Type*) [Group M] [TopologicalSpace M] [IsTopologicalGroup M] [hM : IsOpenUnits M]
-    [Group N] [TopologicalSpace N] [IsTopologicalGroup N] [hN : IsOpenUnits N] :
-    IsOpenUnits (M × N) := by
-  rw [isOpenUnits_iff] at *
-  exact (Topology.IsOpenEmbedding.of_comp_iff _ (hM.prodMap hN)).mpr
-    ContinuousMulEquiv.prodUnits_isOpenEmbedding
-
 theorem approximation_units (S : Finset Nat.Primes) :
     Dense (Set.range (Units.map (finiteEmbedding S).toMonoidHom)) := by
   have : IsOpenUnits (ℝ × ((p : ↥S) → ℚ_[p])) := by
@@ -96,8 +67,6 @@ theorem approximation_units (S : Finset Nat.Primes) :
   simp [← Units.val_inj, hur]
 
 end Rat
-
-
 
 
 /-The code needs to be cleaned up (golf, organize namespaces, move general lemmas to right file, etc), but I will leave this to you. To fill in the sorry in approximation_units, you will also need a general product version of the IsOpenUnits instance. I think this should follow similarly to the prod case (and note that Mathlib already contains ContinuousMulEquiv.piUnits ).-/
